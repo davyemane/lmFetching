033fee6571be47fb3dc682ae466254d0605ef63b
Emane Bile Felicien Davy <82854992+davyemane@users.noreply.github.com>
2023-03-18 14:03:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+var xValues = ["lingala", "bulu", "tété", "Kituba", "Éton"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Langue en cours d'apprentissage"
+    }
+  }
+});
+
+
+
+
 const url_Delete = window.location.search.split("?").join("")
 
 const loginForm = document.getElementById('login-form');
@@ -82,14 +107,13 @@ function writeTocontainer(data) {
              
             //data.map((values)=>{
 
-                data1+= `<a href="contenu.html?url=${data[i].url}">
-                ${data[i].NomChamp}</a> 
-                <a href="editChamp.html?${data[i].url_update}">
-                $update</a>
-                <a href="index.html?${data[i].url_Delete}">
-                Delete</a>
-
-                <br>`
+                data1+= `<div class="col " style="height: 25%; width: 25%;">
+                <button style="border: none; background: none;" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="getContent(${data[i].id_ChampLexicale})">
+                    <img src="img/house.png" class="p-3 btn-circle img-thumbnail " id="img" style=" height: 80%; width: 40%; margin-bottom: 0%;" alt="img" >
+                    <p class="text-center" style="color: black;">${data[i].NomChamp}</p>
+                </button>
+            </div>
+`
         i=i+1
                 }
                 champ.innerHTML=data1
@@ -100,9 +124,63 @@ function writeTocontainer(data) {
 
     }
 
-
 getChamp()
 
+function getContent(urls) {
+
+
+    const endpoint = `http://localhost:8000/formation/champ/?NomChamp=${urls}`;
+    const options = {
+        method: "GET",
+        headers:{
+            "Content-Type":"application/json",
+        }
+    }
+    fetch(endpoint, options)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data);
+        writeToModal(data);
+    })
+}
+
+// fonction qui ecrit dans le champ approprié
+function writeToModal(data) {
+
+if (data.length==0 ) {
+    alert('ce champ est vide')
+} else {
+    if (content) {
+        let data1=""
+        let data2 =""
+        var i=0  
+        console.log(data.length)     
+        while ( i < data.length) {
+            
+             
+            //data.map((values)=>{
+
+                data1+= `<div>
+                <h3>Mot Francais :${data[i].mots.ecriture}</h3>
+                <p>${data[i].mots.Traduction[0].motTraduit}</p>
+                <p style="font-style: italic;">${data[i].corpus}</p>
+                <audio src="${data[i].mots.Traduction[0].audio}" controls></audio>
+              </div>
+    
+                `
+        i=i+1
+                }
+                content.innerHTML=data1
+                if (NomChamps) {
+                    NomChamps.innerHTML= data[0].champ
+                }
+            }
+
+}
+//i=i+1
+  //  }
+
+}
 
 
 function SupChamp(e) {
@@ -127,5 +205,3 @@ function SupChamp(e) {
         console.log('erreur', error)
     })
 }
-
-SupChamp()
